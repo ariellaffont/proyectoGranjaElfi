dff17d91e2a81f8f17da9727bfb30f0367ff0b41
Ariel <ariellaffont@gmail.com>
2024-01-25 18:27:53 +1000

    borrando comentarios y detalles

--- js/pedidos.js
+++ js/pedidos.js
@@ -1,200 +1,188 @@
-let costoEnvio = 0
-let direccion = ""
-let envio = ""
-let metodoPago = ""
-let carrito = []
-let productos = [
-    { id: 1, nombre: 'Milanesas de pollo x kg', precio: 300, categoria: 'Pollo' },
-    { id: 2, nombre: 'Pechugas de pollo x kg', precio: 200, categoria: 'Pollo' },
-    { id: 3, nombre: 'Milanesas de soja x kg', precio: 100, categoria: 'Vegetariano' },
-    { id: 4, nombre: 'Bastoncitos de lentejas x kg', precio: 50, categoria: 'Vegetariano' },
-];
 
 
+let productos = []
 
-function preguntarNombre() {
-    let nombre = prompt('Bienvenido a Granja Elfi, ingresá tu nombre por favor.')
-    mostrarMensaje(`Hola ${nombre}, en que podemos ayudarte?`)
+const obtenerProductos = async () => {
+    const resp = await fetch("../public/data/productos.json");
+    const data = await resp.json();
+    productos = [...data];
+    listarProductos();
 }
+obtenerProductos()
 
-function mostrarProductos() {
-    let texto = ``
+
+let carrito = localStorage.getItem("carrito") == null ? [] : JSON.parse(localStorage.getItem("carrito"))
+localStorage.getItem("carrito") != null ? mostrarCarrito() : Toastify({
+
+    text: `¡No hay productos en el carrito!`,
+
+    duration: 3000
+
+}).showToast();
+
+function listarProductos() {
+    const container = document.getElementById('productContainer')
+    let html = ''
+
     productos.forEach(producto => {
-        texto += `${producto.id} - ${producto.nombre} - precio: ${producto.precio} - Categoria: ${producto.categoria} \n`
-    });
-    alert(texto)
-}
-
-function mostrarProductosPorCategoria() {
-    let categoria
-    let texto = `Encontramos los siguientes productos en la categoría seleccionada:\n`
-    do {
-        categoria = prompt(`Escribe el nombre de la categoria:
-    Pollo
-    Vegetariano`).toLowerCase()
-    } while (categoria != `pollo` && categoria != `vegetariano`);
-    let resultadoCategoria = productos.filter((producto) => producto.categoria.toLowerCase() == categoria)
-    resultadoCategoria.forEach(producto => {
-        texto += `${producto.nombre} Precio $${producto.precio}\n`
-    });
-    alert(texto)
-}
-
-function mostrarMenu() {
-    let opcion = prompt(`Elija una función del menu. Ingrese el numero
-    1) Ver productos
-    2) Ver productos por categoria
-    3) Agregar productos al carrito
-    4) Ver carrito
-    5) Elegir método de envío
-    6) Pagar
-    7) Salir`)
-
-    switch (parseInt(opcion)) {
-        case 1:
-            mostrarProductos()
-            volverAlMenu()
-            break;
-
-        case 2:
-            mostrarProductosPorCategoria()
-            volverAlMenu()
-            break;
-
-        case 3:
-            agregarAlCarrito()
-            mostrarMenu()
-            break
-
-        case 4:
-            verCarrito()
-            mostrarMenu()
-            break
-
-        case 5:
-            metodoEnvio()
-            mostrarMenu()
-            break
-
-        case 6:
-            pagar()
-            break
-
-        case 7:
-            mostrarMensaje('Gracias por su visita')
-            break
-
-        default:
-            if (isNaN(opcion)) {
-                mostrarMensaje('Ha ingresado un caracter. Por favor, solo ingrese numeros')
-            } else {
-                mostrarMensaje('Ha ingresado un numero invalido. Ingrese un numero entre 1 y 7')
-            }
-            mostrarMenu()
-            break;
-    }
+        html += `<div class="cardProducto">
+        <img src="${producto.image}" alt="${producto.name}"
+            class="tamanioImagenPedidos" />
+        <p class="tituloCard">
+            ${producto.name}
+        </p>
+        <p>
+            U$D ${producto.price}
+        </p>
+        <button class="botonCarrito" onclick='agregarAlCarrito("${producto.id}", "${producto.name}")'>
+            Agregar al carrito
+        </button>
+    </div>`
+    })
+    container.innerHTML = html
 
 }
 
-function mostrarMensaje(texto1, texto2 = '') {
-    alert(`${texto1} ${texto2}`)
-}
 
-function volverAlMenu() {
-    let valor = confirm('Desea volver al menu principal?')
-    if (valor) {
-        mostrarMenu()
+
+
+listarProductos()
+
+function agregarAlCarrito(id, nombreProducto) {
+    let resultado = productos.find(producto => producto.id == id)
+    resultado == null ? console.error('Producto no encontrado') : resultado.unidades = resultado.unidades ? resultado.unidades + 1 : 1;
+    let productoEnCarrito = carrito.find(item => item.id === resultado.id);
+    if (productoEnCarrito) {
+
+        productoEnCarrito.unidades = resultado.unidades;
     } else {
-        mostrarMensaje('Gracias por su visita')
+        carrito.push(resultado)
     }
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+    mostrarCarrito()
+    Toastify({
+
+        text: `¡${nombreProducto} se ha agregado al carrito!`,
+
+        duration: 3000
+
+    }).showToast();
 }
 
-function agregarAlCarrito() {
 
-    let cant
-    let opcion
-    let encontrado
 
-    do {
-        mostrarProductos()
-        opcion = parseInt(prompt(`Ingrese el numero del producto que quiere comprar.`))
-        encontrado = productos.find((producto) => producto.id == opcion);
-    } while (encontrado == undefined);
-    cant = pedirCantidad()
-    carrito.push({ ...encontrado, cantidad: cant })
+function eliminarProducto(id) {
+    let resultado = carrito.findIndex(producto => producto.id == id)
+    resultado === -1 ? console.error('Producto no encontrado') : carrito.splice(resultado, 1)
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+    mostrarCarrito()
+
 }
 
-function pedirCantidad() {
-
-    let numero = parseInt(prompt('Elija la cantidad deseada'))
-    return numero
-}
-
-function verCarrito() {
+function mostrarCarrito() {
+    let container = document.getElementById(`listadoCarrito`)
+    let html = ''
     let total = 0
     let unidades = 0
-    let texto = `El carrito contiene estos productos: \n`
-    if (carrito.length == 0) {
-        mostrarMensaje(`No hay ningún producto agregado en el carrito`)
-    }
-    else {
-        carrito.forEach(producto => {
-            total += producto.precio * producto.cantidad
-            unidades += producto.cantidad
-            texto += `Nombre: ${producto.nombre} - Cantidad:${producto.cantidad} - Precio unitario:$${producto.precio} \n`
-        });
-        texto += `
-        Total unidades: ${unidades}
-        Total a pagar: $${total}
-        `
-        alert(texto)
+
+    carrito.forEach(producto => {
+        unidades += producto.unidades;
+        html += `                
+        <tr>
+        <td><img src="${producto.image}" alt="${producto.name}" width="32px" height="32px"></td>
+        <td>${producto.name}</td>
+        <td>U$D ${producto.price}</td>
+        <td>${producto.unidades} Unidades</td>
+        <td><button class="eliminar" onclick='eliminarProducto("${producto.id}")'>Eliminar</button></td>
+    </tr>`
+
+        total += producto.price * producto.unidades;
+    })
+    html += `                
+    <tr>
+    <td></td>
+    <td></td>
+    <td>Total: </td>
+    <td>U$D ${total.toFixed(2)}</td>
+</tr>`
+
+
+
+    container.innerHTML = html
+}
+
+document.getElementById('name').addEventListener("blur", validarFormulario)
+document.getElementById('address').addEventListener("blur", validarFormulario)
+document.getElementById('tel').addEventListener("blur", validarFormulario)
+
+function validarFormulario() {
+    if (document.getElementById("name").value != "" &&
+        document.getElementById("address").value != "" &&
+        document.getElementById("tel").value != "") {
+
+        document.getElementById("completarBtn").disabled = false
+    } else {
+        document.getElementById("completarBtn").disabled = true
+
     }
 }
 
-function metodoEnvio() {
-    let metodo = prompt(`Escriba TA para Take Away o DE para Delivery`).toUpperCase()
-    if (metodo == "TA") {
-        envio = "Take Away"
-        mostrarMensaje(`Seleccionaste satisfactoriamente Take Away`)
-    }
-    else if (metodo == "DE") {
-        envio = "Delivery"
-        mostrarMensaje(`Seleccionaste satisfactoriamente Delivery`)
-        direccion = prompt(`Ingrese su dirección`)
-        mostrarMensaje(`Muchas gracias!`)
-    }
-    else {
-        mostrarMensaje(`Seleccionaste un metodo invalido, asegurate de escribir TA para Take Away o DE para Delivery`)
-        metodoEnvio()
-    }
-}
+document.addEventListener('DOMContentLoaded', () => {
 
-function pagar() {
-    alert(`Tu pedido está casi listo! Por favor asegurate que todos los datos sean correctos:`)
-    verCarrito()
-    let confirmacion = confirm(`El metodo de envio seleccionado es ${envio}
-La direccion ingresada es ${direccion}
-Por si o por no, estos datos son correctos?`)
-    if (confirmacion) {
-        let metodo = parseInt(prompt(`selecciona el metodo de pago
-1. Tarjeta de crédito
-2. Efectivo`))
-        if (metodo == 1) {
-            metodoPago = "Tarjeta de crédito"
-            alert(`Seleccionaste satisfactoriamente Tarjeta de crédito`)
-        }
-        else if (metodo == 2) {
-            metodoPago = "Efectivo"
-            alert(`Seleccionaste satisfactoriamente Efectivo`)
-        }
-        else {
-            mostrarMensaje(`Seleccione un numero valido`)
-            pagar()
-        }
-    }
-    mostrarMensaje(`Muchas gracias por tu compra`)
-    volverAlMenu()
-}
+    const completarBtn = document.querySelector('#completarBtn');
 
-preguntarNombre()
-mostrarMenu()
+    completarBtn.addEventListener("click", (evento) => {
+        Swal.fire({
+            title: 'Confirma para finalizar el pedido, serás redirigido a nuestro chat de whatsapp',
+            text: 'Quiere continuar?',
+            timer: 20000,
+            timerProgressBar: true,
+            confirmButtonColor: "green",
+            confirmButtonText: 'Si',
+            showCancelButton: true,
+            cancelButtonText: "Cancelar",
+            cancelButtonColor: "red",
+            imageUrl: "https://cdn.pixabay.com/photo/2020/04/07/17/01/chicks-5014152_1280.jpg",
+            imageWidth: 400
+        }).then((respuesta) => {
+            if (respuesta.isConfirmed) {
+                Swal.fire({
+                    title: `Gracias por tu compra!
+                Te esperamos nuevamente`,
+                    imageUrl: "https://images.pexels.com/photos/6294198/pexels-photo-6294198.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+                    imageWidth: 200,
+                })
+                    .then((respuesta) => {
+                        if (respuesta.isConfirmed) {
+                            enviarCompra();
+                            localStorage.removeItem("carrito");
+                            carrito = [];
+                            mostrarCarrito();
+                            document.getElementById('formulario').reset();
+                        }
+                    })
+            }
+            if (respuesta.isDismissed) {
+                Swal.fire({
+                    title: `Has cancelado la compra,
+                    Vuelve pronto!`,
+                    imageUrl: "https://images.pexels.com/photos/6898855/pexels-photo-6898855.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+                    imageWidth: 200
+                })
+            }
+        })
+    })
+});
+
+function enviarCompra() {
+    let mensaje = `¡Hola Granja Elfi!%0ASoy ${document.getElementById("name").value} y me gustaría hacerte este pedido:%0A%0A`
+    let totalCarrito = 0
+    carrito.forEach(producto => {
+        mensaje += `Producto: *${producto.name}* %0APrecio unitario: $${producto.price}%0ACantidad: ${producto.unidades}%0A%0A`
+        totalCarrito += producto.price * producto.unidades;
+    });
+
+    mensaje += `%0A*Total de la compra: $${totalCarrito.toFixed(2)}*%0A%0AMis datos son:%0ATeléfono:  ${document.getElementById("tel").value}%0ADirección: ${document.getElementById("address").value}%0A¡Muchas gracias!`
+
+    window.open(`https://api.whatsapp.com/send?phone=610493314100&text=${mensaje}`)
+}
